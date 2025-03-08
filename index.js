970e2d499469df15bf27725a9fa2f2249e340a66
Levis <Levis.mwangi@student.moringaschool.com>
2025-03-08 10:45:32 +0300

    commit changes

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(number){
+  let result;
+  let feet=number;
+  number=parseInt(number);
+  if (feet <= 400){
+    result='This one is on me!'
+  }
+  else if (feet > 400 && feet <= 2000){
+    result= 'That will be twenty bucks.'
+  }
+  else if ( feet <= 2500){
+    result='I will gladly take your thirty bucks.'
+  }
+  else if (feet > 2500){
+    result='No can do.'
+  }
+  return result;
+} 
+
+function ternaryCheckCity(city){
+  return city==='NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let response;
+  switch (tip){
+    case 'generous':
+      response= 'Thank you so much.';
+      break;
+    case 'not as generous':
+      response= 'Thank you.';
+      break;
+    default:
+      response='Bye.';
+      break;
+  }
+  return response;
 }
